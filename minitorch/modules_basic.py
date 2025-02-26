fix: broadcast shape mismatch

minitorch/modules_basic.py
@@ -5,6 +5,7 @@ Sequential
 Embedding
 
 """
+
 from typing import Any, Dict, Optional, Sequence, Tuple
 
 import numpy as np
@@ -32,13 +33,13 @@ class Embedding(Module):
             weights : The learnable weights of shape (num_embeddings, embedding_dim) initialized from N(0, 1).
         """
         self.backend = backend
-        self.num_embeddings = num_embeddings # Vocab size
-        self.embedding_dim  = embedding_dim  # Embedding Dimension
+        self.num_embeddings = num_embeddings  # Vocab size
+        self.embedding_dim = embedding_dim  # Embedding Dimension
         ### BEGIN YOUR SOLUTION
         rand_data = np.random.random((num_embeddings, embedding_dim)).astype(np.float32)
         self.weights = Parameter(tensor_from_numpy(rand_data, backend=backend))
         ### END YOUR SOLUTION
-    
+
     def forward(self, x: Tensor):
         """Maps word indices to one-hot vectors, and projects to embedding vectors.
 
@@ -50,13 +51,17 @@ class Embedding(Module):
         """
         bs, seq_len = x.shape
         ### BEGIN YOUR SOLUTION
-        one_hot_embedding = one_hot(x, self.num_embeddings).view(1, bs * seq_len, self.num_embeddings)
-        return (one_hot_embedding @ self.weights.value).view(bs, seq_len, self.embedding_dim)
+        one_hot_embedding = one_hot(x, self.num_embeddings).view(
+            1, bs * seq_len, self.num_embeddings
+        )
+        return (one_hot_embedding @ self.weights.value).view(
+            bs, seq_len, self.embedding_dim
+        )
         ### END YOUR SOLUTION
 
-    
+
 class Dropout(Module):
-    def __init__(self, p_dropout: float=0.1):
+    def __init__(self, p_dropout: float = 0.1):
         super().__init__()
         """During training, randomly zeroes some of the elements of the input tensor with probability :attr:`p_dropout`.
 
@@ -65,21 +70,21 @@ class Dropout(Module):
         """
         self.p_dropout = p_dropout
 
-    def forward(self, x: Tensor) -> Tensor: 
+    def forward(self, x: Tensor) -> Tensor:
         """During training, randomly zero out elements of a tensor and scale by (1 - p_dropout)
-        
-        Args: 
+
+        Args:
             x : Tensor of shape (*)
-        
-        Returns: 
+
+        Returns:
             output : Tensor of shape (*)
         """
         ### BEGIN YOUR SOLUTION
         if not self.training or self.p_dropout == 0:
             return x
 
-        mask = tensor_from_numpy(np.random.binomial(1, 1-self.p_dropout, x.shape))
-        return  (x * mask) / (1 - self.p_dropout)
+        mask = tensor_from_numpy(np.random.binomial(1, 1 - self.p_dropout, x.shape))
+        return (x * mask) / (1 - self.p_dropout)
         ### END YOUR SOLUTION
 
 
@@ -116,24 +121,23 @@ class Linear(Module):
             )
             - scale
         )
-        self.bias = Parameter(
-            (rand((out_size,), backend=backend) * 2 * scale - scale)
-            if bias
-            else zeros((out_size,), backend=backend)
-        )
+        self.bias = Parameter(tensor((out_size,), backend=backend)) if bias else None
         ### END YOUR SOLUTION
 
     def forward(self, x: Tensor):
         """Applies a linear transformation to the incoming data.
-        
-        Args: 
+
+        Args:
             x : Tensor of shape (n, in_size)
-        
+
         Returns:
             output : Tensor of shape (n, out_size)
         """
         ### BEGIN YOUR SOLUTION
-        return x @ self.weights.value + self.bias.value
+        if self.bias is not None:
+            return x @ self.weights.value + self.bias.value
+        else:
+            return x @ self.weights.value
         ### END YOUR SOLUTION
 
 
@@ -158,14 +162,14 @@ class LayerNorm1d(Module):
         ### END YOUR SOLUTION
 
     def forward(self, x: Tensor) -> Tensor:
-        """Applies Layer Normalization over a mini-batch of inputs. 
+        """Applies Layer Normalization over a mini-batch of inputs.
         NOTE: You can assume the input to this layer is a 2D tensor of shape (batch_size, dim)
         You will use implicit broadcasting in miniTorch to use the weight and bias.
-        
-        Input: 
+
+        Input:
             x - Tensor of shape (bs, dim)
-        
-        Output: 
+
+        Output:
             output - Tensor of shape (bs, dim)
         """
         ### BEGIN YOUR SOLUTION